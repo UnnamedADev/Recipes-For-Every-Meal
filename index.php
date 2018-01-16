be0e41c8c9eb22ca2d720aa98a062604ea742fe1
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-01-16 15:54:22 +0100

    v0.0.1
    STRUCTURE
    + Createad index.php and style.css as main files
    HTML
    + Created basic page structure
    + Created basic projects elements
    CSS
    + Created "native" style fixes
    + Created basic elements style

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>v0.0.1 - Recipes For Every Meal</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+       <h1>Recipes for every meal</h1>
+        <h2>Find out your favourite recipe</h2>
+    </header>
+    <section>
+        <div class="formHolder">
+        aaa
+        </div>
+        <div class="results">
+            bbbb
+        </div>
+    </section>
+    <footer>
+        <h2>Enjoyyy!</h2>
+        <p>Site created for all of you!</p>
+    </footer>
+</body>
+</html>
