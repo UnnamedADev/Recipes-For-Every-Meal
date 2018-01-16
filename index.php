94dc573b0a8e439edaf75a71c59327e0b104d606
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-01-16 18:48:26 +0100

    v0.0.2
    STRUCTURE
    + Added example graphic for meal and difficulty choosing
    HTML
    + Added structure of main site and form
    CSS
    + Added style for main site and form
    PHP
    + Created basic script's structure

--- index.php
+++ index.php
@@ -2,20 +2,68 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>v0.0.1 - Recipes For Every Meal</title>
+    <title>v0.0.2 - Recipes For Every Meal</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header>
-       <h1>Recipes for every meal</h1>
-        <h2>Find out your favourite recipe</h2>
-    </header>
+
+    <nav>
+        <a href="index.php">Strona główna</a>
+        <a href="">Wszystkie przepisy</a>
+    </nav>
     <section>
         <div class="formHolder">
-        aaa
+            <form action="index.php" method="post">
+                 <table>
+                     <tbody>
+                         <tr>
+                             <td><img src="img/meal1.png"></td>
+                             <td><img src="img/difficulty1.png"></td>
+                         </tr>
+                         <tr>
+                             <td>
+                                <div class="customSelect">
+                                    <select name="fMeal">
+                                         <option value="breakfast">Breakfast</option>
+                                         <option value="lunch">Lunch</option>
+                                         <option value="dinner">Dinner</option>
+                                     </select>
+                                </div>
+                             </td>
+                             <td>
+                                 <div class="customSelect">
+                                     <select name="fDifficulty">
+                                         <option value="newbie">Newbie</option>
+                                         <option value="basic">Basic</option>
+                                         <option value="nightmare">Nightmare</option>
+                                     </select>
+                                 </div>
+                             </td>
+                         </tr>
+                     </tbody>
+                 </table>
+
+                 <div>
+                     <button type="reset" class="negetiveButton">Resetuj</button>
+                     <button type="submit" class="positiveButton">Filtruj</button>
+                 </div>
+                 
+            </form>
         </div>
+        
         <div class="results">
-            bbbb
+           <!-- SCRIPT / filter -->
+            <?php
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        
+                    $meal = $_POST["fMeal"];
+                    $difficulty = $_POST["fDifficulty"];
+                    
+                    echo $meal;
+                    echo $difficulty;
+                }
+            ?>
+            results here
         </div>
     </section>
     <footer>
